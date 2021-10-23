fb5d6e70a0ef6c105d559fd97b9e2a5ac848cb35
Nick Shirokov <nick_4312@mail.ru>
2021-10-23 21:48:53 +0300

    feat: Реализовано обращение к серверу за данными пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,30 @@
+import api from "../utils/api";
+import React from "react";
+
 function Main(props) {
+
+  const [userName, setUserName] = React.useState(false);
+  const [userDescription, setUserDescription] = React.useState(false);
+  const [userAvatar, setUserAvatar] = React.useState('../images/avatar.webp');
+
+  React.useEffect(() => {
+    api.getUserInfo().then(data => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    })
+  }, []);
 
   return (
     <main className="root__content">
       <section className="profile root__profile">
         <div className="profile__avatar-container" onClick={props.onEditAvatar}>
-          <img className="profile__avatar" src="./images/avatar.webp" alt="Аватар нашего героя" />
+          <img className="profile__avatar" src={userAvatar} alt="Аватар нашего героя" />
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">Коля Широков</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button className="profile__edit button" type="button" aria-label="Редактировать профиль" onClick={props.onEditProfile}></button>
-          <p className="profile__position">Студент</p>
+          <p className="profile__position">{userDescription}</p>
         </div>
         <button className="profile__add-place button" type="button" aria-label="Добавить карточку места" onClick={props.onAddPlace}></button>
       </section>
