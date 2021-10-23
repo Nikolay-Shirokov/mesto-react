b70416fdd2a57fae989f2a3a2bf75b349fca06d5
Nick Shirokov <nick_4312@mail.ru>
2021-10-23 20:58:31 +0300

    feat: PopupWithForm выделен в компоненту

--- src/components/App.js
+++ src/components/App.js
@@ -2,90 +2,85 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 
+import PopupWithForm from "./PopupWithForm";
+
 function App() {
   return (
     <div className="wrapper">
+
       <Header />
-
       <Main />
-
       <Footer />
 
-      <div className="popup" id="popup-edit-profile">
-        <div className="popup__container">
-          <button className="popup__close button" type="button" aria-label="Закрыть"></button>
-          <form className="form" action="post" name="form-edit-profile" noValidate>
-            <h2 className="form__title">Редактировать профиль</h2>
-            <fieldset className="form__fields">
-              <label className="form__field">
-                <input className="form__input" type="text" name="name" placeholder="Имя героя" required minLength="2" maxLength="40"/>
-                <span className="form__input-error" data-input-name="name"></span>
-              </label>
-              <label className="form__field">
-                <input className="form__input" type="text" name="about" placeholder="Позиция героя в мире" required minLength="2" maxLength="200"/>
-                <span className="form__input-error" data-input-name="about"></span>
-              </label>
-            </fieldset>
-            <button className="form__submit button" type="submit" data-waiting-text="Сохренение...">Сохранить</button>
-          </form>
-        </div>
-      </div>
+      <PopupWithForm
+        title="Редактировать профиль"
+        name="edit-profile"
+        fieldset={(
+          <fieldset className="form__fields">
+            <label className="form__field">
+              <input className="form__input" type="text" name="name" placeholder="Имя героя" required minLength="2" maxLength="40" />
+              <span className="form__input-error" data-input-name="name"></span>
+            </label>
+            <label className="form__field">
+              <input className="form__input" type="text" name="about" placeholder="Позиция героя в мире" required minLength="2" maxLength="200" />
+              <span className="form__input-error" data-input-name="about"></span>
+            </label>
+          </fieldset>
+        )}
+        buttonText="Сохранить"
+        buttonWaitingText="Сохранение..."
+      />
 
-      <div className="popup" id="popup-add-place">
-        <div className="popup__container">
-          <button className="popup__close button" type="button" aria-label="Закрыть"></button>
-          <form className="form" action="post" name="form-add-place" noValidate>
-            <h2 className="form__title">Новое место</h2>
-            <fieldset className="form__fields">
-              <label className="form__field">
-                <input className="form__input" type="text" name="name" placeholder="Название" required minLength="2" maxLength="30"/>
-                <span className="form__input-error" data-input-name="name"></span>
-              </label>
-              <label className="form__field">
-                <input className="form__input" type="url" name="link" placeholder="Ссылка на картинку" required/>
-                <span className="form__input-error" data-input-name="link"></span>
-              </label>
-            </fieldset>
-            <button className="form__submit button" type="submit" data-waiting-text="Создание...">Создать</button>
-          </form>
-        </div>
-      </div>
+      <PopupWithForm
+        title="Новое место"
+        name="add-place"
+        fieldset={(
+          <fieldset className="form__fields">
+            <label className="form__field">
+              <input className="form__input" type="text" name="name" placeholder="Название" required minLength="2" maxLength="30" />
+              <span className="form__input-error" data-input-name="name"></span>
+            </label>
+            <label className="form__field">
+              <input className="form__input" type="url" name="link" placeholder="Ссылка на картинку" required />
+              <span className="form__input-error" data-input-name="link"></span>
+            </label>
+          </fieldset>
+        )}
+        buttonText="Создать"
+        buttonWaitingText="Создание..."
+      />
+
+      <PopupWithForm
+        title="Вы уверены?"
+        name="accept"
+        buttonText="Да"
+      />
+
+      <PopupWithForm
+        title="Обновить аватар"
+        name="edit-avatar"
+        fieldset={(
+          <fieldset className="form__fields">
+            <label className="form__field">
+              <input className="form__input" type="url" name="link" placeholder="Ссылка на картинку" required />
+              <span className="form__input-error" data-input-name="link"></span>
+            </label>
+          </fieldset>
+        )}
+        buttonText="Сохранить"
+        buttonWaitingText="Сохренение..."
+      />
 
       <div className="popup" id="popup-picture">
         <div className="popup__container">
           <button className="popup__close button" type="button" aria-label="Закрыть"></button>
           <figure className="figure">
-            <img className="figure__image" src="./images/yandex-praktikum.svg" alt="Логотип Яндекс.Практикум"/>
+            <img className="figure__image" src="./images/yandex-praktikum.svg" alt="Логотип Яндекс.Практикум" />
             <figcaption className="figure__caption">Яндекс.Практикум</figcaption>
           </figure>
         </div>
       </div>
 
-      <div className="popup" id="popup-accept">
-        <div className="popup__container">
-          <button className="popup__close button" type="button" aria-label="Закрыть"></button>
-          <form className="form" action="post" name="form-accept" noValidate>
-            <h2 className="form__title">Вы уверены?</h2>
-            <button className="form__submit button" type="submit">Да</button>
-          </form>
-        </div>
-      </div>
-
-      <div className="popup" id="popup-edit-avatar">
-        <div className="popup__container">
-          <button className="popup__close button" type="button" aria-label="Закрыть"></button>
-          <form className="form" action="post" name="form-edit-avatar" noValidate>
-            <h2 className="form__title">Обновить аватар</h2>
-            <fieldset className="form__fields">
-              <label className="form__field">
-                <input className="form__input" type="url" name="link" placeholder="Ссылка на картинку" required/>
-                <span className="form__input-error" data-input-name="link"></span>
-              </label>
-            </fieldset>
-            <button className="form__submit button" type="submit" data-waiting-text="Сохренение...">Сохранить</button>
-          </form>
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,16 @@
+function PopupWithForm(props) {
+  return (
+    <div className="popup" id={`popup-${props.name}`}>
+      <div className="popup__container">
+        <button className="popup__close button" type="button" aria-label="Закрыть"></button>
+        <form className="form" action="post" name={`form-${props.name}`} noValidate>
+          <h2 className="form__title">{props.title}</h2>
+          {props.fieldset}
+          <button className="form__submit button" type="submit" data-waiting-text={props.buttonWaitingText}>{props.buttonText}</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
