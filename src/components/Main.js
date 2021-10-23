111cdd8306a7deaaee678c86c535727599923e91
Nick Shirokov <nick_4312@mail.ru>
2021-10-23 20:27:55 +0300

    feat: Добавлены имеративные обработчики открытия попапов

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,36 @@
 function Main(props) {
+
+  function openPopup(popup) {
+    popup.classList.add('popup_opened');
+  }
+
+  function handleEditAvatarClick() {
+    const popup = document.querySelector('#popup-edit-avatar');
+    openPopup(popup);
+  }
+
+  function handleEditProfileClick() {
+    const popup = document.querySelector('#popup-edit-profile');
+    openPopup(popup);
+  }
+
+  function handleAddPlaceClick() {
+    const popup = document.querySelector('#popup-add-place');
+    openPopup(popup);
+  }
+
   return (
     <main className="root__content">
       <section className="profile root__profile">
-        <div className="profile__avatar-container">
+        <div className="profile__avatar-container" onClick={handleEditAvatarClick}>
           <img className="profile__avatar" src="./images/avatar.webp" alt="Аватар нашего героя" />
         </div>
         <div className="profile__info">
           <h1 className="profile__name">Коля Широков</h1>
-          <button className="profile__edit button" type="button" aria-label="Редактировать профиль"></button>
+          <button className="profile__edit button" type="button" aria-label="Редактировать профиль" onClick={handleEditProfileClick}></button>
           <p className="profile__position">Студент</p>
         </div>
-        <button className="profile__add-place button" type="button" aria-label="Добавить карточку места"></button>
+        <button className="profile__add-place button" type="button" aria-label="Добавить карточку места" onClick={handleAddPlaceClick}></button>
       </section>
       <section className="root__places">
         <ul className="places">
