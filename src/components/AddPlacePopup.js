7271220de52291a3b4a66843c70e115b26ef07ec
Nick Shirokov <nick_4312@mail.ru>
2021-11-14 14:29:27 +0300

    feat: Определен хук для валидации и работы с формами

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,47 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useFormAndValidation } from "../hooks/useFormAndValidation";
 import PopupWithForm from "./PopupWithForm";
 
 function AddPlacePopup(props) {
 
-  const [name, setName] = useState('');
-  const [link, setLink] = useState('');
-
-  const [nameValidity, setNameValidity] = useState({valid: true, message: ''});
-  const [linkValidity, setLinkValidity] = useState({valid: true, message: ''});
-
-  function setStateValidity(input, validityState, setValidityState) {
-    const valid = input.validity.valid;
-    const message = input.validationMessage;
-    if (valid !== validityState.valid || message !== validityState.message) {
-      setValidityState({valid, message})
-    }
-  }
-
-  function handleNameChange(event) {
-    setName(event.target.value);
-    setStateValidity(event.target, nameValidity, setNameValidity);
-  }
-
-  function handleLinkChange(event) {
-    setLink(event.target.value);
-    setStateValidity(event.target, linkValidity, setLinkValidity);
-  }
+  const {values, handleChange, errors, isValid, resetForm} = useFormAndValidation()
 
   function handleSubmit() {
     const result = props.onAddCard({
-      name,
-      link,
+      name: values.name,
+      link: values.link,
     });
     return result;
   }
 
   useEffect(() => {
-    if (!props.isOpen) return;
-    setName('');
-    setLink('');
-    setNameValidity({valid: false, message: ''});
-    setLinkValidity({valid: false, message: ''});
-  }, [props.isOpen])
+    if (props.isOpen) {
+      resetForm();
+    };
+  }, [props.isOpen, resetForm])
 
   return (
     <PopupWithForm
@@ -54,16 +31,16 @@ function AddPlacePopup(props) {
       fieldset={(
         <fieldset className="form__fields">
           <label className="form__field">
-            <input value={name} onChange={handleNameChange} className={`form__input ${!nameValidity.message? '': 'form__input_type_error'}`} type="text" name="name" placeholder="Название" required minLength="2" maxLength="30" />
-            <span className={`form__input-error ${!nameValidity.message? '': 'form__input-error_visible'}`} data-input-name="name">{nameValidity.message}</span>
+            <input value={values.name || ''} onChange={handleChange} className={`form__input ${!errors.name? '': 'form__input_type_error'}`} type="text" name="name" placeholder="Название" required minLength="2" maxLength="30" />
+            <span className={`form__input-error ${!errors.name? '': 'form__input-error_visible'}`} data-input-name="name">{errors.name}</span>
           </label>
           <label className="form__field">
-            <input value={link} onChange={handleLinkChange} className={`form__input ${!linkValidity.message? '': 'form__input_type_error'}`} type="url" name="link" placeholder="Ссылка на картинку" required />
-            <span className={`form__input-error ${!linkValidity.message? '': 'form__input-error_visible'}`} data-input-name="link">{linkValidity.message}</span>
+            <input value={values.link || ''} onChange={handleChange} className={`form__input ${!errors.link? '': 'form__input_type_error'}`} type="url" name="link" placeholder="Ссылка на картинку" required />
+            <span className={`form__input-error ${!errors.link? '': 'form__input-error_visible'}`} data-input-name="link">{errors.link}</span>
           </label>
         </fieldset>
       )}
-      isButtonDisabled = {!(nameValidity.valid && linkValidity.valid)}
+      isButtonDisabled = {!isValid}
       buttonText="Создать"
       buttonWaitingText="Создание..."
     />
