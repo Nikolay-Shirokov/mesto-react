b52a3585912ec6a7349b09205d7ce31956c4b989
Nick Shirokov <nick_4312@mail.ru>
2021-11-14 15:22:32 +0300

    feat: Подключена валидация к форме редактирования профиля

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,35 +1,31 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useFormAndValidation } from "../hooks/useFormAndValidation";
 import PopupWithForm from "./PopupWithForm";
 
 function EditProfilePopup(props) {
 
-  const [name, setName] = useState('');
-  const [about, setAbout] = useState('');
+  const { values, handleChange, errors, isValid, resetForm } = useFormAndValidation();
+  const formRef = useRef();
 
   const currentUser = useContext(CurrentUserContext);
   // После загрузки текущего пользователя из API
   // его данные будут использованы в управляемых компонентах.
   useEffect(() => {
     if (currentUser.name) {
-      setName(currentUser.name);
-      setAbout(currentUser.about);
+      const newValues = {
+        name: currentUser.name,
+        about: currentUser.about
+      }
+      resetForm(newValues, {}, formRef.current.checkValidity());
     }
-  }, [currentUser, props.isOpen]);
-
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleAboutChange(event) {
-    setAbout(event.target.value);
-  }
+  }, [currentUser, props.isOpen, resetForm]);
 
   function handleSubmit() {
     // Передаём значения управляемых компонентов во внешний обработчик
     const result = props.onUpdateUser({
-      name,
-      about,
+      name: values.name,
+      about: values.about,
     });
     return result;
   }
@@ -39,20 +35,22 @@ function EditProfilePopup(props) {
       {...props}
       title="Редактировать профиль"
       name="edit-profile"
+      formRef={formRef}
       isOpen={props.isOpen}
       onClose={props.onClose}
       onSubmit={handleSubmit}
+      isButtonDisabled={!isValid}
       buttonText="Сохранить"
       buttonWaitingText="Сохранение..."
     >
       <fieldset className="form__fields">
         <label className="form__field">
-          <input value={name} onChange={handleNameChange} className="form__input" type="text" name="name" placeholder="Имя героя" required minLength="2" maxLength="40" />
-          <span className="form__input-error" data-input-name="name"></span>
+          <input value={values.name || ''} onChange={handleChange} className={`form__input ${!errors.name ? '' : 'form__input_type_error'}`} type="text" name="name" placeholder="Имя героя" required minLength="2" maxLength="40" />
+          <span className={`form__input-error ${!errors.name ? '' : 'form__input-error_visible'}`}>{errors.name}</span>
         </label>
         <label className="form__field">
-          <input value={about} onChange={handleAboutChange} className="form__input" type="text" name="about" placeholder="Позиция героя в мире" required minLength="2" maxLength="200" />
-          <span className="form__input-error" data-input-name="about"></span>
+          <input value={values.about || ''} onChange={handleChange} className={`form__input ${!errors.about ? '' : 'form__input_type_error'}`} type="text" name="about" placeholder="Позиция героя в мире" required minLength="2" maxLength="200" />
+          <span className={`form__input-error ${!errors.about ? '' : 'form__input-error_visible'}`}>{errors.about}</span>
         </label>
       </fieldset>
     </PopupWithForm>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -20,7 +20,7 @@ function PopupWithForm(props) {
 
   return (
     <Popup {...props}>
-      <form className="form" action="post" name={`form-${props.name}`} onSubmit={handleSubmitForm} noValidate>
+      <form ref={props.formRef} className="form" action="post" name={`form-${props.name}`} onSubmit={handleSubmitForm} noValidate>
         <h2 className="form__title">{props.title}</h2>
         {props.children}
         <button className={`form__submit ${props.isButtonDisabled? 'form__submit_disabled': ''} button`} type="submit">{buttonText}</button>
